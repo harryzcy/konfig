9878790f4cd3ac9e2784d5d76a36d792c3f2997b
Chongyi Zheng <git@zcy.dev>
2023-11-24 02:04:52 -0500

    Setup nextjs for cloudflare pages (#3)

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen p-12">
+      <h1 className="text-xl text-center mb-4">Konfig</h1>
+      <p className="text-center">Centralized configuration infrastructure</p>
+    </main>
+  )
+}

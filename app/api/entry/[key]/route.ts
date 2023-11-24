e8381d214456c91d1439296d219bab69f6561ef7
Chongyi Zheng <git@zcy.dev>
2023-11-24 17:09:49 -0500

    Update handler

--- app/api/entry/[key]/route.ts
+++ app/api/entry/[key]/route.ts
@@ -5,7 +5,7 @@ import { NextApiRequest } from 'next'
 
 export const runtime = 'edge'
 
-export async function GET(req: NextApiRequest) {
+const handler = async (req: NextApiRequest) => {
   let key: ConfigKey
   try {
     key = parseConfigKey(req.query.key as string)
@@ -14,36 +14,45 @@ export async function GET(req: NextApiRequest) {
     return errorResponse(error)
   }
 
-  const { CONFIG_KV } = process.env as unknown as Env
+  if (req.method === 'GET') {
+    const { CONFIG_KV } = process.env as unknown as Env
 
-  const raw = await CONFIG_KV.get(`entry:${key}`)
-  if (raw === null) {
-    return new Response('{"message":"not found"}', {
+    const raw = await CONFIG_KV.get(`entry:${key}`)
+    if (raw === null) {
+      return new Response('{"message":"not found"}', {
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8'
+        },
+        status: 404
+      })
+    }
+
+    const value = parseConfigValue(raw)
+    const entry = { ...value, key } as ConfigEntry
+
+    return jsonResponse(entry)
+  } else if (req.method === 'DELETE') {
+    let key: ConfigKey
+    try {
+      key = parseConfigKey(req.query.key as string)
+    } catch (e) {
+      const error = e as Error
+      return errorResponse(error)
+    }
+
+    const { CONFIG_KV } = process.env as unknown as Env
+
+    await CONFIG_KV.delete(`entry:${key}`)
+
+    return successResponse()
+  } else {
+    return new Response('{"message":"method not allowed"}', {
       headers: {
         'Content-Type': 'application/json; charset=UTF-8'
       },
-      status: 404
+      status: 405
     })
   }
-
-  const value = parseConfigValue(raw)
-  const entry = { ...value, key } as ConfigEntry
-
-  return jsonResponse(entry)
 }
 
-export async function DELETE(req: NextApiRequest) {
-  let key: ConfigKey
-  try {
-    key = parseConfigKey(req.query.key as string)
-  } catch (e) {
-    const error = e as Error
-    return errorResponse(error)
-  }
-
-  const { CONFIG_KV } = process.env as unknown as Env
-
-  await CONFIG_KV.delete(`entry:${key}`)
-
-  return successResponse()
-}
+export default handler

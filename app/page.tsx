ba8ffd8b3eee5169eddfeabfd037a6d3ddf21167
Chongyi Zheng <git@zcy.dev>
2024-01-27 23:53:21 -0500

    Update sidebar style

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import GroupNav from './components/GroupNav'
 export default function Home() {
   return (
     <main className="min-h-screen p-4 flex">
-      <div>
+      <div className="select-none w-60">
         <div>
           <h1 className="text-xl text-center mb-4">Konfig</h1>
         </div>

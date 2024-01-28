a1775f6259010accc2dd26a730de0a7640d123b3
Chongyi Zheng <git@zcy.dev>
2024-01-28 09:19:49 +0800

    Configure cloudflare kv binding for next.js (#138)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
+
+if (process.env.NODE_ENV === 'development') {
+  // import the utility from the next-dev submodule
+  const { setupDevBindings } = require('@cloudflare/next-on-pages/next-dev')
+
+  // call the utility with the bindings you want to have access to
+  setupDevBindings({
+    bindings: {
+      CONFIG_KV: {
+        type: 'kv',
+        id: 'CONFIG_KV'
+      }
+    }
+  })
+}
+
 const nextConfig = {}
 
 module.exports = nextConfig

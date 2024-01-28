bdc0b9cb29d80ee8c41777bae353e1a567c1e3f1
Chongyi Zheng <git@zcy.dev>
2024-01-28 01:04:00 -0500

    Render groups

--- app/components/GroupNav.tsx
+++ app/components/GroupNav.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { PlusIcon } from '@heroicons/react/24/outline'
 import useSWR from 'swr'
 
 interface GroupResponse {
@@ -10,25 +13,28 @@ export default function GroupNav() {
     return (await res.json()) as GroupResponse
   })
 
-  if (isLoading) {
-    return <div>Loading...</div>
-  }
-
-  if (error) {
-    return <div>Failed to load</div>
-  }
-
-  if (!data || data.groups.length === 0) {
-    return <div>No groups</div>
-  }
-
   return (
-    <nav>
+    <div className="p-2">
+      <div className="flex justify-between items-center text-sm pb-2">
+        <span>Groups</span>
+        <span className="p-1.5 cursor-pointer rounded-md hover:bg-gray-100 hover:dark:bg-gray-700">
+          <PlusIcon className="w-4 h-4" />
+        </span>
+      </div>
+
+      {isLoading && <div className="italic">Loading...</div>}
+
+      {!isLoading && error && <div className="italic">Failed to load</div>}
+
+      {!isLoading && (!data || data.groups.length === 0) && (
+        <div className="italic">No groups</div>
+      )}
+
       {data?.groups.map((group) => (
         <div key={group}>
           <span>{group}</span>
         </div>
       ))}
-    </nav>
+    </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-'use client'
-
 import GroupNav from './components/GroupNav'
 
 export default function Home() {
   return (
-    <main className="min-h-screen p-4 flex">
-      <div className="select-none w-60">
-        <div>
-          <h1 className="text-xl text-center mb-4">Konfig</h1>
+    <main className="min-h-screenflex">
+      <div className="select-none w-60 py-4">
+        <div className="px-2">
+          <h1 className="text-xl text-center">Konfig</h1>
         </div>
 
         <GroupNav />

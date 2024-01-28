6fbe47597a26f045ebe63f47f983cb09da700c1d
Chongyi Zheng <git@zcy.dev>
2024-01-28 04:05:09 -0500

    Add shadcn

--- app/components/GroupNav.tsx
+++ app/components/GroupNav.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { PlusIcon } from '@heroicons/react/24/outline'
+import { useState } from 'react'
 import useSWR from 'swr'
 
 interface GroupResponse {
@@ -13,11 +14,19 @@ export default function GroupNav() {
     return (await res.json()) as GroupResponse
   })
 
+  const [isDraftActive, setIsDraftActive] = useState<boolean>(false)
+  const [draftGroup, setDraftGroup] = useState<string>('')
+
   return (
     <div className="p-2">
       <div className="flex justify-between items-center text-sm pb-2">
         <span>Groups</span>
-        <span className="p-1.5 cursor-pointer rounded-md hover:bg-gray-100 hover:dark:bg-gray-700">
+        <span
+          className="p-1.5 cursor-pointer rounded-md hover:bg-gray-100 hover:dark:bg-gray-700"
+          onClick={() => {
+            setIsDraftActive(true)
+          }}
+        >
           <PlusIcon className="w-4 h-4" />
         </span>
       </div>
@@ -26,8 +35,14 @@ export default function GroupNav() {
 
       {!isLoading && error && <div className="italic">Failed to load</div>}
 
-      {!isLoading && (!data || data.groups.length === 0) && (
+      {!isLoading && (!data || data.groups.length === 0) && !isDraftActive && (
         <div className="italic">No groups</div>
+      )}
+
+      {isDraftActive && (
+        <div className="w-full h-10 border rounded-md">
+          <span>{draftGroup}</span>
+        </div>
       )}
 
       {data?.groups.map((group) => (

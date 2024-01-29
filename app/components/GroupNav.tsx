dc4cfd3db5d27c58ad5e26238a8828213c0654a1
Chongyi Zheng <git@zcy.dev>
2024-01-28 23:32:50 -0500

    Add new group form

--- app/components/GroupNav.tsx
+++ app/components/GroupNav.tsx
@@ -1,12 +1,31 @@
 'use client'
 
+import { useOutsideClick } from '../hooks/useOutsideClick'
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
 import { PlusIcon } from '@heroicons/react/24/outline'
-import { useState } from 'react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useRef, useState } from 'react'
+import { useForm } from 'react-hook-form'
 import useSWR from 'swr'
+import { z } from 'zod'
 
 interface GroupResponse {
   groups: string[]
 }
+
+const NewGroupFormSchema = z.object({
+  group: z.string().min(1, {
+    message: 'Group name should not be empty'
+  })
+})
 
 export default function GroupNav() {
   const { data, error, isLoading } = useSWR('/api/groups', async (url) => {
@@ -40,9 +59,11 @@ export default function GroupNav() {
       )}
 
       {isDraftActive && (
-        <div className="w-full h-10 border rounded-md">
-          <span>{draftGroup}</span>
-        </div>
+        <NewGroupForm
+          cancel={() => {
+            setIsDraftActive(false)
+          }}
+        />
       )}
 
       {data?.groups.map((group) => (
@@ -51,5 +72,49 @@ export default function GroupNav() {
         </div>
       ))}
     </div>
+  )
+}
+
+function NewGroupForm(props: { cancel: () => void }) {
+  const newGroupForm = useForm<z.infer<typeof NewGroupFormSchema>>({
+    resolver: zodResolver(NewGroupFormSchema),
+    defaultValues: {
+      group: ''
+    }
+  })
+
+  const onSubmit = (data: z.infer<typeof NewGroupFormSchema>) => {
+    console.log('submit')
+  }
+
+  const formRef = useRef<HTMLFormElement>(null)
+  const submitRef = useRef<HTMLInputElement>(null)
+  useOutsideClick(formRef, () => {
+    if (newGroupForm.getValues().group === '') {
+      newGroupForm.reset()
+      props.cancel()
+    } else {
+      submitRef.current?.click()
+    }
+  })
+
+  return (
+    <Form {...newGroupForm}>
+      <form onSubmit={newGroupForm.handleSubmit(onSubmit)} ref={formRef}>
+        <FormField
+          control={newGroupForm.control}
+          name="group"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="Type group name here" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <input className="hidden" type="submit" ref={submitRef} />
+      </form>
+    </Form>
   )
 }

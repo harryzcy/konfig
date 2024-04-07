32353dc15271de3af6b9ebbae2fb8a574413bdcd
Chongyi Zheng <git@zcy.dev>
2024-04-07 17:19:59 -0400

    Add api to list environments (#348)

--- app/api/environments/route.test.ts
+++ app/api/environments/route.test.ts
@@ -1,10 +1,42 @@
-import { POST } from './route'
+import { GET, POST } from './route'
 import { Env } from '@/src/types'
 import { createRequest } from '@/test/utils'
 import { describe } from 'node:test'
 import { expect, test } from 'vitest'
 
 const bindings = getMiniflareBindings() as Env
+
+describe('GET /api/environments', () => {
+  const url = '/api/environments'
+
+  test('empty', async () => {
+    const req = createRequest('GET', url, null)
+    const res = await GET(req)
+    expect(res).toHaveProperty('status', 200)
+    expect(await res.json()).toEqual({ environments: [] })
+  })
+
+  test('non-empty', async () => {
+    {
+      const req = createRequest(
+        'POST',
+        '/api/environments',
+        '{"name":"production"}'
+      )
+      const res = await POST(req)
+      expect(res).toHaveProperty('status', 200)
+    }
+
+    const req = createRequest('GET', url, null)
+    const res = await GET(req)
+    expect(res).toHaveProperty('status', 200)
+    const result = await res.json()
+    expect(result).toHaveProperty('environments')
+    expect(result['environments']).toHaveLength(1)
+    expect(result['environments']).toBeInstanceOf(Array)
+    expect(result['environments']).toContain('production')
+  })
+})
 
 describe('POST /api/environments', () => {
   test('success', async () => {

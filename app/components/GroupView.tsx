2eb935ab270b51015832bc9c6e015eff6166d179
Chongyi Zheng <git@zcy.dev>
2024-04-11 16:34:10 -0400

    Update UI

--- app/components/GroupView.tsx
+++ app/components/GroupView.tsx
@@ -4,12 +4,12 @@ import GroupLinkEnvironment from './GroupLinkEnvironment'
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger
 } from '@/components/ui/dialog'
 import { Group } from '@/src/types'
+import { PlusIcon } from '@heroicons/react/24/outline'
 import useSWR from 'swr'
 
 export interface GroupViewProps {
@@ -93,10 +93,21 @@ export function GroupViewEnvironmentsMenu(
   }
 
   return (
-    <div>
+    <div className="inline-flex p-1 h-10 items-center justify-center rounded-md bg-muted text-muted-foreground w-full">
       {environments.map((env) => {
-        return <div key={env}>{env}</div>
+        return (
+          <div
+            key={env}
+            data-state="active"
+            className="flex-1 inline-flex items-center cursor-pointer justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm"
+          >
+            <span>{env}</span>
+          </div>
+        )
       })}
+      <div className="px-2 py-1.5 cursor-pointer">
+        <PlusIcon className="w-4 h-4 text-muted-foreground" />
+      </div>
     </div>
   )
 }

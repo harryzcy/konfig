d60ebd2f2deb0e5715818a4cb249e92bf5df90cf
Chongyi Zheng <git@zcy.dev>
2024-09-14 14:07:15 -0400

    Use js config

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+const config = {
+  trailingComma: 'none',
+  tabWidth: 4,
+  semi: false,
+  singleQuote: true,
+  plugins: [
+    'prettier-plugin-tailwindcss',
+    '@trivago/prettier-plugin-sort-imports'
+  ]
+}
+
+export default config

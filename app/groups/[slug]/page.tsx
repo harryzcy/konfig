513821e80fb5202f4c57da47c9fc90dcc22a8bca
Chongyi Zheng <git@zcy.dev>
2024-10-30 20:35:48 -0400

    Use router to get slug

--- app/groups/[slug]/page.tsx
+++ app/groups/[slug]/page.tsx
@@ -1,16 +1,19 @@
 'use client'
 
+import { useRouter } from 'next/router';
+
 import GroupView from '@/app/components/GroupView'
 import SideBar from '@/app/components/SideBar'
 import { Toaster } from '@/components/ui/toaster'
 
 export const runtime = 'edge'
 
-export default function Page({ params }: { params: { slug: string } }) {
+export default function Page() {
+  const router = useRouter()
   return (
     <div className="min-h-screen flex">
       <SideBar />
-      <GroupView groupName={params.slug} />
+      <GroupView groupName={router.query.slug as string} />
 
       <Toaster />
     </div>

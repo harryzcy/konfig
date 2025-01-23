ea6fb20eae5a1dc651cc5090c927abffac660eb2
Chongyi Zheng <git@zcy.dev>
2025-01-23 00:28:13 -0500

    Run tailwindcss upgrade tool

--- app/components/GroupView.tsx
+++ app/components/GroupView.tsx
@@ -99,7 +99,7 @@ export function GroupViewEnvironmentsMenu(
           <div
             key={env}
             data-state="active"
-            className="flex-1 inline-flex items-center cursor-pointer justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm"
+            className="flex-1 inline-flex items-center cursor-pointer justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-xs"
           >
             <span>{env}</span>
           </div>

2e059191e8e2598a446f73e8f23c134b20df87b2
Chongyi Zheng <git@zcy.dev>
2025-03-04 23:52:27 -0500

    Resolve `@` in vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { cloudflare } from '@cloudflare/vite-plugin'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), cloudflare()],
   define: {
     'process.env': {}
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   }
 })

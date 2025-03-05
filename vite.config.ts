a8fe638816ccb4b95743ccccb1ffd44357752e3e
Chongyi Zheng <git@zcy.dev>
2025-03-04 23:57:01 -0500

    Transform with hono

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { cloudflare } from '@cloudflare/vite-plugin'
+import build from '@hono/vite-cloudflare-pages'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), cloudflare()],
+  plugins: [react(), tailwindcss(), cloudflare(), build()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')

016fd7b8154fd8bd3c181b3459c60922454152b1
Chongyi Zheng <git@zcy.dev>
2025-03-05 02:11:59 -0500

    Fix some tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,34 @@
+// import { defineConfig } from 'vitest/config'
+// export default defineConfig({
+//   plugins: [react()],
+//   test: {
+//     globals: true,
+//     environment: 'miniflare',
+//     environmentOptions: {
+//       kvNamespaces: ['CONFIG_KV']
+//     }
+//   },
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname)
+//     }
+//   }
+// })
+import { defineWorkersConfig } from '@cloudflare/vitest-pool-workers/config'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { defineConfig } from 'vitest/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-export default defineConfig({
-  plugins: [react()],
+export default defineWorkersConfig({
+  plugins: [tsconfigPaths(), react()],
   test: {
-    globals: true,
-    environment: 'miniflare',
-    environmentOptions: {
-      kvNamespaces: ['CONFIG_KV']
+    coverage: {
+      provider: 'istanbul'
+    },
+    poolOptions: {
+      workers: {
+        wrangler: { configPath: './wrangler.toml' }
+      }
     }
   },
   resolve: {

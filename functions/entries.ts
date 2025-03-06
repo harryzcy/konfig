be9b78514f5db14664f362d34151397326440cb0
Chongyi Zheng <git@zcy.dev>
2025-03-06 03:52:05 -0500

    Add entries

---
+++ functions/entries.ts
@@ -0,0 +1,26 @@
+import { parseConfigEntry } from '@/parse'
+import { errorResponse, successResponse } from '@/response'
+import type { ConfigEntry, ConfigValue, Env } from '@/types'
+
+export const onRequestPost: PagesFunction<Env> = async (context) => {
+  console.log('Handling POST request')
+
+  let entry: ConfigEntry
+  try {
+    entry = parseConfigEntry(await context.request.text())
+  } catch (e) {
+    const error = e as Error
+    return errorResponse(error)
+  }
+
+  const value = {
+    type: entry.type,
+    value: entry.value
+  } as ConfigValue
+
+  console.log(`Storing key entry:${entry.key} in KV`)
+  await context.env.CONFIG_KV.put(`entry:${entry.key}`, JSON.stringify(value))
+  console.log(`Stored key entry:${entry.key} in KV`)
+
+  return successResponse()
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { cloudflare } from '@cloudflare/vite-plugin'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import path from 'node:path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({

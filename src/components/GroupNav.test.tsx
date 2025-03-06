3f70c13cb1db148a49e2408515abec2ac434277f
Chongyi Zheng <git@zcy.dev>
2025-03-06 02:45:27 -0500

    Import testing lib vitest

--- src/components/GroupNav.test.tsx
+++ src/components/GroupNav.test.tsx
@@ -1,12 +1,14 @@
 // @vitest-environment jsdom
 import GroupNav from './GroupNav'
 import '@testing-library/jest-dom'
-import '@testing-library/jest-dom/vitest'
+import * as matchers from '@testing-library/jest-dom/matchers'
 import { render, screen, waitFor } from '@testing-library/react'
 import user from '@testing-library/user-event'
 import { http, HttpResponse } from 'msw'
 import { setupServer } from 'msw/node'
 import { afterAll, afterEach, beforeAll, describe, expect, test } from 'vitest'
+
+expect.extend(matchers)
 
 const server = setupServer(
   http.get('/api/groups', () => {
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,19 +1,3 @@
-// import { defineConfig } from 'vitest/config'
-// export default defineConfig({
-//   plugins: [react()],
-//   test: {
-//     globals: true,
-//     environment: 'miniflare',
-//     environmentOptions: {
-//       kvNamespaces: ['CONFIG_KV']
-//     }
-//   },
-//   resolve: {
-//     alias: {
-//       '@': path.resolve(__dirname)
-//     }
-//   }
-// })
 import { defineWorkersConfig } from '@cloudflare/vitest-pool-workers/config'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'

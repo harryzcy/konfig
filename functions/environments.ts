cf6619a3c1b51b6086ffa5df99ed3c27fb49ed32
Chongyi Zheng <git@zcy.dev>
2025-03-06 04:15:30 -0500

    Add environments endpoint

---
+++ functions/environments.ts
@@ -0,0 +1,54 @@
+import { parseNewEnvironmentRequest } from '@/parse'
+import { errorResponse, jsonResponse, successResponse } from '@/response'
+import { getUnixTimestamp } from '@/time'
+import type {
+  Env,
+  NewEnvironmentRequest,
+  EnvironmentMetadata,
+  EnvironmentValue
+} from '@/types'
+
+export const onRequestGet: PagesFunction<Env> = async (context) => {
+  console.log('Handling GET request')
+
+  const prefix = 'env:'
+  console.log(`Getting key with prefix ${prefix} from KV`)
+  const value = await context.env.CONFIG_KV.list<EnvironmentMetadata>({
+    prefix
+  })
+  console.log(`Got key with prefix ${prefix} from KV`)
+
+  const environments = value.keys.map((key) => key.name.replace(prefix, ''))
+
+  return jsonResponse({
+    environments: environments
+  })
+}
+
+export const onRequestPost: PagesFunction<Env> = async (context) => {
+  console.log('Handling POST request')
+
+  let env: NewEnvironmentRequest
+  try {
+    env = parseNewEnvironmentRequest(await context.request.text())
+  } catch (e) {
+    const error = e as Error
+    return errorResponse(error)
+  }
+
+  const key = `env:${env.name}`
+  const value = {
+    groups: []
+  } as EnvironmentValue
+  const metadata = {
+    created: getUnixTimestamp()
+  } as EnvironmentMetadata
+
+  console.log(`Storing key ${key} in KV`)
+  await context.env.CONFIG_KV.put(key, JSON.stringify(value), {
+    metadata
+  })
+  console.log(`Stored key ${key} in KV`)
+
+  return successResponse()
+}

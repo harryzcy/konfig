0edb807b1d3f6539440edd666a3061b40861e8df
Chongyi Zheng <git@zcy.dev>
2025-03-12 04:19:04 -0400

    Switch to use sonner

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
-import GroupEmpty from './components/GroupEmpty'
-import SideBar from './components/SideBar'
+import GroupEmpty from '@/components/GroupEmpty'
+import SideBar from '@/components/SideBar'
+import { Toaster } from '@/components/ui/sonner'
 
 export default function App() {
   return (
     <div className="min-h-screen flex">
       <SideBar />
       <GroupEmpty />
+      <Toaster />
     </div>
   )
 }

3df36af359757b2675b1d5c9175ebcc8c5e560a4
Chongyi Zheng <git@zcy.dev>
2025-10-25 17:16:50 -0500

    Add api routing

--- src/index.tsx
+++ src/index.tsx
@@ -9,4 +9,12 @@ app.get('/', (c) => {
   return c.render(<h1>Hello!</h1>)
 })
 
+const api = new Hono()
+
+api.get('/ping', (c) => {
+  return c.json({ message: 'pong' })
+})
+
+app.route('/api', api)
+
 export default app

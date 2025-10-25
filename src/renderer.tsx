d61da0e97be98a27c8b23fcdcfebaba0ffca51f9
Chongyi Zheng <git@zcy.dev>
2025-10-25 16:48:38 -0500

    Update html

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,14 +1,21 @@
-import { jsxRenderer } from 'hono/jsx-renderer'
-import { Link, ViteClient } from 'vite-ssr-components/hono'
+import { jsxRenderer } from "hono/jsx-renderer";
+import { Link, ViteClient } from "vite-ssr-components/hono";
 
 export const renderer = jsxRenderer(({ children }) => {
   return (
     <html>
       <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Konfig</title>
+        <meta
+          name="description"
+          content="Centralized configuration infrastructure"
+        />
         <ViteClient />
         <Link href="/src/style.css" rel="stylesheet" />
       </head>
       <body>{children}</body>
     </html>
-  )
-})
+  );
+});

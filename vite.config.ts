620424a579c59442f70f013d740fb7729fefc3ea
Chongyi Zheng <git@zcy.dev>
2025-10-25 23:41:24 -0500

    Resolve @ path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { cloudflare } from '@cloudflare/vite-plugin'
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import ssrPlugin from 'vite-ssr-components/plugin'
 
 export default defineConfig({
-  plugins: [cloudflare(), ssrPlugin()]
+  plugins: [cloudflare(), ssrPlugin()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })

a52a353d3586e6a0931e3f5bd68dc3859f515fd7
Chongyi Zheng <git@zcy.dev>
2025-10-25 19:15:48 -0500

    Use vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,11 @@
-import {
-  buildPagesASSETSBinding,
-  defineWorkersConfig
-} from '@cloudflare/vitest-pool-workers/config'
-import path from 'node:path'
+// import { defineWorkersConfig } from '@cloudflare/vitest-pool-workers/config'
+import { defineConfig } from 'vitest/config'
 
-const assetsPath = path.join(__dirname, 'public')
+// import path from 'node:path'
 
-export default defineWorkersConfig({
+// const assetsPath = path.join(__dirname, 'public')
+
+export default defineConfig({
   test: {
     coverage: {
       provider: 'istanbul'

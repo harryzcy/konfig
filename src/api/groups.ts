7d0a64fb523ca95b4650255efdcaf0b0c0a882bf
Chongyi Zheng <git@zcy.dev>
2025-10-25 19:26:50 -0500

    Bring over groups APIs (#1999)

--- src/api/groups.ts
+++ src/api/groups.ts
@@ -1,11 +1,48 @@
-import { Context } from 'hono'
+import { ContextWithBindings } from '../common/bindings'
+import { parseNewGroupRequest } from '../common/parse'
+import { errorResponse, successResponse } from '../common/response'
+import { getUnixTimestamp } from '../common/time'
+import { GroupMetadata, GroupValue, NewGroupRequest } from '../common/types'
 
-export const groupsGet = (c: Context) => {
+export const groupsGet = async (c: ContextWithBindings) => {
   console.log('Handling GET request for groups')
-  return c.json({ groups: [] })
+
+  const prefix = 'group:'
+  console.log(`Getting key with prefix ${prefix} from KV`)
+  const value = await c.env.CONFIG_KV.list<GroupMetadata>({
+    prefix
+  })
+  console.log(`Got key with prefix ${prefix} from KV`)
+
+  const groups = value.keys.map((key) => key.name.replace(prefix, ''))
+
+  return c.json({ groups })
 }
 
-export const groupsPost = (c: Context) => {
+export const groupsPost = async (c: ContextWithBindings) => {
   console.log('Handling POST request for groups')
-  return c.json({ message: 'success' })
+
+  let env: NewGroupRequest
+  try {
+    env = parseNewGroupRequest(await c.req.text())
+  } catch (e) {
+    const error = e as Error
+    return errorResponse(c, error)
+  }
+
+  const key = `group:${env.name}`
+  const value = {
+    environments: []
+  } as GroupValue
+  const metadata = {
+    created: getUnixTimestamp()
+  } as GroupMetadata
+
+  console.log(`Storing key ${key} in KV`)
+  await c.env.CONFIG_KV.put(key, JSON.stringify(value), {
+    metadata
+  })
+  console.log(`Stored key ${key} in KV`)
+
+  return successResponse(c)
 }

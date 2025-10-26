2540947d86ed0169bfbfa478262575394fc74e10
Chongyi Zheng <git@zcy.dev>
2025-10-25 19:22:07 -0500

    Disable some tests in cloudflare environment (#1998)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,11 @@
-import {
-  buildPagesASSETSBinding,
-  defineWorkersConfig
-} from '@cloudflare/vitest-pool-workers/config'
-import path from 'node:path'
+// import { defineWorkersConfig } from '@cloudflare/vitest-pool-workers/config'
+import { defineConfig } from 'vitest/config'
 
-const assetsPath = path.join(__dirname, 'public')
+// import path from 'node:path'
 
-export default defineWorkersConfig({
+// const assetsPath = path.join(__dirname, 'public')
+
+export default defineConfig({
   test: {
     coverage: {
       provider: 'istanbul'
@@ -15,39 +14,39 @@ export default defineWorkersConfig({
       enabled: true,
       tsconfig: 'tsconfig.test.json'
     },
-    poolOptions: {
-      workers: {
-        wrangler: { configPath: './wrangler.jsonc' },
-        miniflare: {
-          kvNamespaces: ['CONFIG_KV'],
-          serviceBindings: {
-            ASSERTS: await buildPagesASSETSBinding(assetsPath)
-          }
-        }
-      }
-    },
+    // poolOptions: {
+    //   workers: {
+    //     wrangler: { configPath: './wrangler.jsonc' },
+    //     miniflare: {
+    //       kvNamespaces: ['CONFIG_KV'],
+    //       serviceBindings: {
+    //         ASSERTS: await buildPagesASSETSBinding(assetsPath)
+    //       }
+    //     }
+    //   }
+    // },
     projects: [
       {
         // Vite code
         test: {
-          include: ['src/*.test.ts', 'src/**/*.test.ts'],
-          environment: 'jsdom',
-          pool: 'forks',
-          setupFiles: ['./jest-setup.ts']
+          include: ['src/*.test.ts', 'src/**/*.test.ts']
+          // environment: 'jsdom',
+          // pool: 'forks'
+          // setupFiles: ['./jest-setup.ts']
         },
         resolve: {
           alias: {
             '@': new URL('./src/', import.meta.url).pathname
           }
         }
-      },
-      {
-        // Cloudflare Pages Functions code
-        extends: true,
-        test: {
-          include: ['tests/*.test.ts', 'tests/**/*.test.ts']
-        }
       }
+      // {
+      //   // Cloudflare Pages Functions code
+      //   extends: true,
+      //   test: {
+      //     include: ['tests/*.test.ts', 'tests/**/*.test.ts']
+      //   }
+      // }
     ]
   },
   resolve: {

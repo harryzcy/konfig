ead1a469a4042dfae5d2f643d238517ff2d78aa8
Chongyi Zheng <git@zcy.dev>
2025-10-26 20:40:19 -0500

    Bring over all environments tests

--- tests/environments.test.ts
+++ tests/environments.test.ts
@@ -1,4 +1,5 @@
 import app from '../src'
+import { TTL } from '../src/common/constants'
 import { env } from 'cloudflare:test'
 import { describe, expect, test } from 'vitest'
 
@@ -113,5 +114,159 @@ describe('GET /api/environments/[name]', () => {
     expect(await res.text()).toBe(
       '{"error":"invalid input: key cannot contain \\":\\""}'
     )
+  })
+})
+
+describe('DELETE /api/environments/[name]', () => {
+  test('success', async () => {
+    await env.CONFIG_KV.put('env:production', JSON.stringify({ groups: [] }), {
+      metadata: {
+        created: 123,
+        deleted: Math.floor(Date.now() / 1000 - TTL - 1)
+      }
+    })
+
+    const res = await app.request(
+      '/api/environments/production',
+      { method: 'DELETE' },
+      env
+    )
+    expect(res.status).toBe(200)
+    expect(await res.text()).toBe('{"message":"success"}')
+
+    await expect(env.CONFIG_KV.get('env:production')).resolves.toBe(null)
+
+    // Deleting again should give 404
+    const resAgain = await app.request(
+      '/api/environments/production',
+      { method: 'DELETE' },
+      env
+    )
+    expect(resAgain).toHaveProperty('status', 404)
+  })
+
+  test('not soft deleted', async () => {
+    await env.CONFIG_KV.put('env:production', JSON.stringify({ groups: [] }), {
+      metadata: {
+        created: 123
+      }
+    })
+
+    const res = await app.request(
+      '/api/environments/production',
+      { method: 'DELETE' },
+      env
+    )
+    expect(res).toHaveProperty('status', 400)
+    expect(await res.text()).toBe(
+      '{"error":"Environment production is not soft deleted"}'
+    )
+  })
+
+  test('soft deleted within TTL', async () => {
+    await env.CONFIG_KV.put('env:production', JSON.stringify({ groups: [] }), {
+      metadata: {
+        created: 123,
+        deleted: Math.floor(Date.now() / 1000)
+      }
+    })
+
+    const res = await app.request(
+      '/api/environments/production',
+      { method: 'DELETE' },
+      env
+    )
+    expect(res).toHaveProperty('status', 400)
+    expect(await res.text()).toBe(
+      '{"error":"Environment production is soft deleted within TTL"}'
+    )
+  })
+
+  test('has groups', async () => {
+    await env.CONFIG_KV.put(
+      'env:production',
+      JSON.stringify({ groups: ['project-1'] }),
+      {
+        metadata: {
+          created: 123,
+          deleted: Math.floor(Date.now() / 1000 - TTL - 1)
+        }
+      }
+    )
+
+    const res = await app.request(
+      '/api/environments/production',
+      { method: 'DELETE' },
+      env
+    )
+    expect(res).toHaveProperty('status', 400)
+    expect(await res.text()).toBe(
+      '{"error":"Environment production has groups"}'
+    )
+  })
+
+  test('invalid name', async () => {
+    const res = await app.request(
+      '/api/environments/invalid:key',
+      { method: 'DELETE' },
+      env
+    )
+    expect(res).toHaveProperty('status', 400)
+    expect(await res.text()).toBe(
+      '{"error":"invalid input: key cannot contain \\":\\""}'
+    )
+  })
+})
+
+describe('POST /api/environments/[name]/delete', () => {
+  const url = '/api/environments/production/delete'
+
+  test('success', async () => {
+    await env.CONFIG_KV.put('env:production', JSON.stringify({ groups: [] }), {
+      metadata: {
+        created: 123
+      }
+    })
+
+    const res = await app.request(url, { method: 'POST' }, env)
+    expect(res.status).toBe(200)
+    expect(await res.text()).toBe('{"message":"success"}')
+
+    await expect(env.CONFIG_KV.get('env:production')).resolves.toBeDefined()
+
+    // Deleting again should give 400, already soft deleted
+    const resAgain = await app.request(url, { method: 'POST' }, env)
+    expect(resAgain).toHaveProperty('status', 400)
+    expect(await resAgain.text()).toBe(
+      '{"error":"Environment production is already soft deleted"}'
+    )
+  })
+
+  test('has groups', async () => {
+    await env.CONFIG_KV.put(
+      'env:production',
+      JSON.stringify({ groups: ['project-1'] }),
+      {
+        metadata: {
+          created: 123
+        }
+      }
+    )
+
+    const res = await app.request(url, { method: 'POST' }, env)
+    expect(res).toHaveProperty('status', 400)
+    expect(await res.text()).toBe(
+      '{"error":"Environment production has groups"}'
+    )
+  })
+
+  test('404 not found', async () => {
+    const res = await app.request(
+      '/api/environments/not-found/delete',
+      { method: 'POST' },
+      env
+    )
+    expect(res).toHaveProperty('status', 404)
+    expect(await res.text()).toBe('{"error":"not found"}')
   })
 })

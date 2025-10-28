fee209371e3b7f1debd8944cc39b781913026a1b
Chongyi Zheng <git@zcy.dev>
2025-10-27 22:34:38 -0500

    Try inserting in renderer

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,19 +1,33 @@
 import { reactRenderer } from '@hono/react-renderer'
+import { css, Style } from 'hono/css'
 
-export const renderer = reactRenderer(({ children }) => {
-  return (
-    <html>
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Konfig</title>
-        <meta
-          name="description"
-          content="Centralized configuration infrastructure"
-        />
-        <link href="/src/style.css" rel="stylesheet" />
-      </head>
-      <body>{children}</body>
-    </html>
-  )
-})
+export const renderer = reactRenderer(
+  ({ children }) => {
+    const globalClass = css`
+      @import 'tailwindcss';
+    `
+
+    return (
+      <html>
+        <head>
+          <meta charSet="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <title>Konfig</title>
+          <meta
+            name="description"
+            content="Centralized configuration infrastructure"
+          />
+          <link href="/src/style.css" rel="stylesheet" />
+          <Style>{globalClass}</Style>
+        </head>
+        <body>{children}</body>
+      </html>
+    )
+  },
+  {
+    stream: true
+  }
+)

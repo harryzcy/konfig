8db93beb9f7496826e888ebdbfe184a2ef49994d
Chongyi Zheng <git@zcy.dev>
2025-11-08 20:50:19 -0600

    Do client build seperately

--- vite.config.ts
+++ vite.config.ts
@@ -7,25 +7,67 @@ import react from '@vitejs/plugin-react'
 import path from 'node:path'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  ssr: {
-    external: ['react', 'react-dom']
-  },
-  plugins: [
-    build({
-      entry: 'src/index.tsx'
-    }),
-    react(),
-    tailwindcss(),
-    cloudflare(),
-    devServer({
-      adapter,
-      entry: 'src/index.tsx'
-    })
-  ],
-  resolve: {
+export default defineConfig(({ command, mode }) => {
+  const resolve = {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  }
+
+  if (command === 'serve') {
+    return {
+      ssr: {
+        external: ['react', 'react-dom']
+      },
+      build: {
+        manifest: false
+      },
+      plugins: [
+        build({
+          entry: 'src/index.tsx'
+        }),
+        react(),
+        tailwindcss(),
+        cloudflare(),
+        devServer({
+          adapter,
+          entry: 'src/index.tsx'
+        })
+      ],
+      resolve
+    }
+  }
+
+  if (mode === 'client') {
+    return {
+      build: {
+        manifest: true,
+        rollupOptions: {
+          input: ['./src/style.css'],
+          output: {
+            assetFileNames: 'assets/[name].[ext]'
+          }
+        }
+      },
+      resolve
+    }
+  }
+
+  return {
+    ssr: {
+      external: ['react', 'react-dom']
+    },
+    build: {
+      manifest: false
+    },
+    plugins: [
+      build({
+        entry: 'src/index.tsx'
+      }),
+      react(),
+      tailwindcss()
+      // cloudflare()
+    ],
+    resolve
   }
 })
